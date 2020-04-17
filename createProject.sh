8d04b85110f8eb4384c11f0483a0b75b391cefeb
Andrew Lvovsky <alvov001@ucr.edu>
2020-04-17 11:55:32 -0700

    hotfix for mmcu and freq vars

--- createProject.sh
+++ createProject.sh
@@ -60,8 +60,8 @@ EOF
 # Create the Makefile
 echo '# University of California, Riverside' > "$name"/Makefile
 echo '# CS120B Makefile' >> "$name"/Makefile
-echo 'MMCU=$mmcu' >> "$name"/Makefile
-echo 'FREQ=$freq' >> "$name"/Makefile
+echo "MMCU=${mmcu}" >> "$name"/Makefile
+echo "FREQ=${freq}" >> "$name"/Makefile
 cat $SCRIPTDIR/templates/MakefileTemplate >> "$name"/Makefile
 
 # Create Simulator header
